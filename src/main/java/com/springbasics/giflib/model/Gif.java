6ab4c19023117832a7cf0e2784f753c78c9a1f54
Wiktor <wiktor.boniecki@gmail.com>
2017-07-26 14:12:44 +0200

    update to course repo files

--- src/main/java/com/springbasics/giflib/model/Gif.java
+++ src/main/java/com/springbasics/giflib/model/Gif.java
@@ -1,43 +1,77 @@
 package com.springbasics.giflib.model;
 
-import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
 
 public class Gif {
-    private String name;
-    private int categoryId;
-    private LocalDate dateUploaded;
-    private String username;
+    private Long id;
+    private byte[] bytes;
+    private String description;
+    private Category category;
+    private LocalDateTime dateUploaded = LocalDateTime.now();
+    private String username = "You";
     private boolean favorite;
+    private String hash;
 
-    public Gif(String name, int categoryId, LocalDate dateUploaded, String username, boolean favorite) {
-        this.name = name;
-        this.categoryId = categoryId;
-        this.dateUploaded = dateUploaded;
-        this.username = username;
-        this.favorite = favorite;
+    public Gif(){}
+
+    public String getTimeSinceUploaded() {
+        String unit = "";
+        LocalDateTime now = LocalDateTime.now();
+        long diff;
+        if((diff = ChronoUnit.SECONDS.between(dateUploaded,now)) < 60){
+            unit = "secs";
+        } else if ((diff = ChronoUnit.MINUTES.between(dateUploaded,now)) < 60) {
+            unit = "mins";
+        } else if ((diff = ChronoUnit.HOURS.between(dateUploaded,now)) < 24) {
+            unit = "hours";
+        } else if ((diff = ChronoUnit.DAYS.between(dateUploaded,now)) < 30) {
+            unit = "days";
+        } else if ((diff = ChronoUnit.MONTHS.between(dateUploaded,now)) < 12) {
+            unit = "months";
+        } else{
+            diff = ChronoUnit.YEARS.between(dateUploaded,now);
+        }
+        return String.format("%d %s",diff,unit);
     }
 
-    public int getCategoryId() {
-        return categoryId;
+    public Long getId() {
+        return id;
     }
 
-    public void setCategoryId(int categoryId) {
-        this.categoryId = categoryId;
+    public void setId(Long id) {
+        this.id = id;
     }
 
-    public String getName() {
-        return name;
+    public byte[] getBytes() {
+        return bytes;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setBytes(byte[] bytes) {
+        this.bytes = bytes;
     }
 
-    public LocalDate getDateUploaded() {
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+    public LocalDateTime getDateUploaded() {
         return dateUploaded;
     }
 
-    public void setDateUploaded(LocalDate dateUploaded) {
+    public void setDateUploaded(LocalDateTime dateUploaded) {
         this.dateUploaded = dateUploaded;
     }
 
@@ -55,5 +89,13 @@ public class Gif {
 
     public void setFavorite(boolean favorite) {
         this.favorite = favorite;
+    }
+
+    public String getHash() {
+        return hash;
+    }
+
+    public void setHash(String hash) {
+        this.hash = hash;
     }
 }
