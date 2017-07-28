d2637a3a0f2781b9224ad84fd372a4c305bf57b4
Wiktor <wiktor.boniecki@gmail.com>
2017-07-28 22:07:18 +0200

    add a gif dao and service class and interface :beer:

---
+++ src/main/java/com/springbasics/giflib/dao/GifDaoImpl.java
@@ -0,0 +1,62 @@
+package com.springbasics.giflib.dao;
+
+import com.springbasics.giflib.model.Gif;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+import java.util.List;
+
+@Repository
+public class GifDaoImpl implements GifDao {
+    @Autowired
+    private SessionFactory sessionFactory;
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public List<Gif> findAll() {
+               // Open a session
+        Session session = sessionFactory.openSession();
+
+        //Get all categories with Hibernate criteria object
+        CriteriaBuilder builder = session.getCriteriaBuilder();
+        CriteriaQuery<Gif> criteria = builder.createQuery(Gif.class);
+        Root<Gif> varRoot = criteria.from(Gif.class);
+        criteria.select(varRoot);
+        List<Gif> gifs = session.createQuery(criteria).getResultList();
+
+        // Close session
+        session.close();
+        return gifs;
+    }
+
+    @Override
+    public Gif findById(Long id) {
+        Session session = sessionFactory.openSession();
+        Gif gif = session.get(Gif.class, id);
+        session.close();
+        return gif;
+    }
+
+    @Override
+    public void save(Gif gif) {
+        Session session = sessionFactory.openSession();
+        session.getTransaction();
+        session.save(gif);
+        session.getTransaction().commit();
+        session.close();
+    }
+
+    @Override
+    public void delete(Gif gif) {
+        Session session = sessionFactory.openSession();
+        session.getTransaction();
+        session.delete(gif);
+        session.getTransaction().commit();
+        session.close();
+    }
+}
---
+++ src/main/java/com/springbasics/giflib/service/GifService.java
@@ -0,0 +1,14 @@
+package com.springbasics.giflib.service;
+
+
+import com.springbasics.giflib.model.Gif;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+
+public interface GifService {
+    List<Gif> findAll();
+    Gif findById(Long id);
+    void save(Gif gif, MultipartFile file);
+    void delete(Gif gif);
+}
